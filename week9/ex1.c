b8f49ab6ebd81a5132efc3e99ccad809eb000a16
Ahmad Alhussin <a.alhussin@innopolis.university>
2022-11-08 20:16:17 +0300

    added week 9

---
+++ week9/ex1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int main(int argc,char *argv[]){
+    int pageFrames;
+    if (argc==1){
+        printf("Enter the number of pageframes: (or add it as a command line argument)\n");
+        scanf("%d",&pageFrames);
+    }
+    else {
+        pageFrames = atoi(argv[1]);
+    }
+    int *pages = malloc(pageFrames*sizeof(int));
+    int *cnt = malloc(pageFrames*sizeof(int));
+    for (int i=0;i<pageFrames;i++){
+        pages[i]=-1;
+        cnt[i]=0;
+    }
+    freopen("input.txt","r",stdin);
+    int request,previousPage=INT_MIN,hit=0,miss=0;
+    while (scanf("%d",&request)!=EOF){
+        for (int i=0;i<pageFrames;i++){
+            cnt[i]>>=1;
+            if (pages[i]==previousPage){
+                cnt[i]|=(1<<7);
+            }
+        }
+        int found=0;
+        for (int i=0;i<pageFrames;i++){
+            if (pages[i]==request){
+                found=1;
+                break;
+            }
+        }
+        if (found==1){
+            hit++;
+            previousPage=request;
+            continue;
+        }
+        else {
+            miss++;
+        }
+        int placed=0;
+        for (int i=0;i<pageFrames;i++){
+            if (pages[i]==-1){
+                pages[i]=request;
+                previousPage=request;
+                cnt[i]=0;
+                placed=1;
+                break;
+            }
+        }
+        if (placed==1)continue;
+        int mnidx=0;
+        for (int i=0;i<pageFrames;i++){
+            if (cnt[i]<cnt[mnidx])mnidx=i;
+        }
+        pages[mnidx]=request;
+        cnt[mnidx]=0;
+        previousPage=request;
+    }
+    printf("Number of hits are: %d, Number of misses are: %d\n",hit,miss);
+    float ratio = hit;
+    ratio = ratio/miss;
+    printf("Hit/Miss ratio: %f\n",ratio);
+
+}
